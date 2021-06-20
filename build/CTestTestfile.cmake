baa904accdfbba5857b50895122b063b2ddebd82
Twim17 <fred17.wallace@gmail.com>
2021-06-20 12:07:16 +0200

    prova

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/me/progetto_labiagi/src
+# Build directory: /home/me/progetto_labiagi/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
